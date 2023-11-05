415e5f7caab8494a01eba17d54646b26371de770
Samuele <samuelevf@gmail.com>
2023-11-05 23:30:06 +0100

    be: update util router

--- backend/hermadata/routers/util.py
+++ backend/hermadata/routers/util.py
@@ -1,16 +1,23 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from hermadata.dependancies import get_session
-
-from hermadata.repositories.city import ProvinciaModel, SQLCityRepository
+from sqlalchemy.orm import Session
+from hermadata.repositories.city import ComuneModel, ProvinciaModel, SQLCityRepository
 
 
 router = APIRouter(prefix="/util")
 
 
 @router.get("/province", response_model=list[ProvinciaModel])
-def get_province():
-    with get_session() as s:
-        repo = SQLCityRepository(s)
+def get_province(session: Session = Depends(get_session)):
+    repo = SQLCityRepository(session)
 
-        province = repo.get_province()
+    province = repo.get_province()
     return province
+
+
+@router.get("/comuni", response_model=list[ComuneModel])
+def get_comuni(provincia: str, session: Session = Depends(get_session)):
+    repo = SQLCityRepository(session)
+
+    comuni = repo.get_comuni(provincia=provincia)
+    return comuni
