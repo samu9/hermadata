6956b3b7f94a616df19aeaf3920b90c28af2c25b
Samuele <samuelevf@gmail.com>
2023-11-05 23:32:35 +0100

    fe: add endpoints

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,4 +1,13 @@
 import axios, { AxiosInstance } from "axios"
+import { NewAnimalSchema } from "../models/new-animal.schema"
+import ApiEndpoints from "./apiEndpoints"
+import {
+    ComuneSchema,
+    ProvinciaSchema,
+    comuneSchema,
+    provinciaSchema,
+} from "../models/city.schema"
+import { RaceSchema, raceSchema } from "../models/race.schema"
 
 class ApiService {
     inst: AxiosInstance
@@ -12,15 +21,51 @@ class ApiService {
         })
     }
 
-    private async get(endpoint: string) {}
+    private async get<T>(
+        endpoint: string,
+        params: { [key: string]: any } = {}
+    ): Promise<T> {
+        const res = await this.inst.get<T>(endpoint, { params })
+        return res.data
+    }
 
     private async post<T>(endpoint: string, data: object): Promise<T> {
         const res = await this.inst.post<T>(endpoint, data)
         return res.data
     }
 
-    createAnimal(data: { name: string }) {
+    getAllRaces() {
+        const result = this.get(ApiEndpoints.race.getAll)
+    }
+    createAnimal(data: NewAnimalSchema) {
         return this.post(ApiEndpoints.animal.create, data)
+    }
+
+    async getProvince(): Promise<ProvinciaSchema[]> {
+        const data = await this.get<ProvinciaSchema[]>(
+            ApiEndpoints.util.getProvince
+        )
+        const result = data.map((d) => provinciaSchema.parse(d))
+
+        return result
+    }
+
+    async getComuni(provincia: string): Promise<ComuneSchema[]> {
+        const data = await this.get<ComuneSchema[]>(
+            ApiEndpoints.util.getComuni,
+            { provincia }
+        )
+        const result = data.map((d) => comuneSchema.parse(d))
+
+        return result
+    }
+
+    async getRaces(): Promise<RaceSchema[]> {
+        const data = await this.get<RaceSchema[]>(ApiEndpoints.race.getAll)
+
+        const result = data.map((d) => raceSchema.parse(d))
+
+        return result
     }
 }
 
