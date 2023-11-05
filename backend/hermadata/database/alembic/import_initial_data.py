0b66bc8559dd795a3a770cefd9b2e930fdc6e6cb
Samuele <samuelevf@gmail.com>
2023-11-05 23:30:20 +0100

    be: import initial races

--- backend/hermadata/database/alembic/import_initial_data.py
+++ backend/hermadata/database/alembic/import_initial_data.py
@@ -3,7 +3,7 @@ import os
 from sqlalchemy import Engine, create_engine, insert, select
 from sqlalchemy.orm import sessionmaker
 from hermadata.settings import Settings
-from hermadata.database.models import Comune, Provincia
+from hermadata.database.models import Comune, Provincia, Race
 
 
 def import_initial_data(stage: str, engine: Engine):
@@ -39,6 +39,18 @@ def import_initial_data(stage: str, engine: Engine):
                         id=r['codice_belfiore'],
                         name=r['denominazione_ita'],
                         provincia=r['sigla_provincia'],
+                    )
+                )
+        with open(os.path.join(initial_data_dir, 'races.csv'), 'r') as fp:
+            reader = csv.DictReader(fp)
+            for i, r in enumerate(reader):
+                check = s.execute(select(Race.code).where(Race.code == r['id'])).first()
+                if check:
+                    continue
+                s.execute(
+                    insert(Race).values(
+                        code=r['id'],
+                        name=r['name'],
                     )
                 )
 
