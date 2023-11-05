65aaae7facf59108f30f0fc7fe9e11745dbd1490
Samuele <samuelevf@gmail.com>
2023-11-05 23:30:57 +0100

    be: update main app

--- backend/hermadata/main.py
+++ backend/hermadata/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
-from hermadata.routers import animal
+from hermadata.routers import animal, util, race
 from hermadata.settings import Settings
+from fastapi.middleware.cors import CORSMiddleware
 
 settings = Settings()
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(animal.router)
+app.include_router(util.router)
+app.include_router(race.router)
