4f1fc9fcb4527cdcc5547670d4ba8255f932d03b
Samuele <samuelevf@gmail.com>
2023-11-05 23:32:42 +0100

    fe: add endpoints map

--- frontend/src/services/apiEndpoints.ts
+++ frontend/src/services/apiEndpoints.ts
@@ -1,5 +1,16 @@
 const ApiEndpoints = {
+    race: {
+        getAll: "/race",
+        getByCode: (code: string) => `/race/${code}`,
+        create: "/race",
+    },
     animal: {
+        getByCode: (code: string) => `/animal/${code}`,
         create: "/animal",
     },
+    util: {
+        getProvince: "/util/province",
+        getComuni: "/util/comuni",
+    },
 }
+export default ApiEndpoints
