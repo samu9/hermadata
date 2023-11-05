915ce8c7923df07f06b2348df276fd631dd83bcf
Samuele <samuelevf@gmail.com>
2023-11-05 23:31:57 +0100

    fe: update main entry point with initializations

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App.tsx"
+import "./index.css"
+import ApiService from "./services/api.ts"
+import { QueryClient, QueryClientProvider } from "react-query"
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+export const apiService = new ApiService(import.meta.env.VITE_API_BASE_URL)
+const queryClient = new QueryClient()
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <App />
+        </QueryClientProvider>
+    </React.StrictMode>
 )
