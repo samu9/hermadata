5247c1a43f2a8a8da7fa43d2e50caadb1be88d55
Samuele <samuelevf@gmail.com>
2023-11-13 21:40:20 +0100

    add rounter

--- backend/hermadata/routers/animal.py
+++ backend/hermadata/routers/animal.py
@@ -1,3 +1,23 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from hermadata.dependancies import get_session
+from sqlalchemy.orm import Session
 
-router = APIRouter()
+from hermadata.repositories.animal import AnimalModel, NewAnimalModel, SQLAnimalRepository
+
+router = APIRouter(prefix="/animal")
+
+
+@router.post("")
+def create_new_animal(data: NewAnimalModel, session: Session = Depends(get_session)):
+    repo = SQLAnimalRepository(session)
+
+    animal_code = repo.generate_code(
+        race_code=data.race, origin_city_code=data.origin_city_code, finding_date=data.finding_date
+    )
+
+    data = {**data.model_dump(), "code": animal_code}
+    animal = AnimalModel.model_validate(data)
+
+    repo.save(animal)
+
+    return animal_code
