cf43f53fdde0922afa9101e4b9f3018d8334c419
Samuele <samuelevf@gmail.com>
2023-11-17 07:27:20 +0100

    fe: fix new animal schema

--- frontend/src/models/new-animal.schema.ts
+++ frontend/src/models/new-animal.schema.ts
@@ -2,7 +2,7 @@ import { z } from "zod"
 
 export const newAnimalSchema = z.object({
     race: z.string().length(1),
-    origin_city_code: z.string().regex(/[A-Z]\d{3/),
+    origin_city_code: z.string().regex(/[A-Z]\d{3}/),
     // sex: z.number(),
     finding_date: z.string().regex(/\d{4}-\d{2}-\d{2}/),
 })
