f8065f7ca0c742570a95f1d93187a1e31ea40937
Samuele <samuelevf@gmail.com>
2023-11-17 07:27:11 +0100

    be: add test

---
+++ backend/tests/repositories/test_get_repository.py
@@ -0,0 +1,8 @@
+from hermadata.dependancies import get_repository
+from hermadata.repositories.animal import SQLAnimalRepository
+
+
+def test_get_repository():
+    repo_factory = get_repository(SQLAnimalRepository)
+    repo = repo_factory()
+    assert repo
