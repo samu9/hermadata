95612e90a258c7b5261e5d6836c44b737774c0de
Samuele <samuelevf@gmail.com>
2023-11-19 23:35:35 +0100

    fe: install prime react

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,8 @@ import App from "./App.tsx"
 import "./index.css"
 import ApiService from "./services/api.ts"
 import { QueryClient, QueryClientProvider } from "react-query"
+import "primereact/resources/themes/lara-light-indigo/theme.css"
+import { PrimeReactProvider } from "primereact/api"
 
 export const apiService = new ApiService(import.meta.env.VITE_API_BASE_URL)
 const queryClient = new QueryClient()
@@ -11,7 +13,9 @@ const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <QueryClientProvider client={queryClient}>
-            <App />
+            <PrimeReactProvider value={{}}>
+                <App />
+            </PrimeReactProvider>
         </QueryClientProvider>
     </React.StrictMode>
 )
