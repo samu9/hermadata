cac3cf446159a87f42a950358269f64363a033d9
Samuele <samuelevf@gmail.com>
2023-11-19 10:55:38 +0100

    be: add animal endpoints

--- backend/hermadata/routers/animal.py
+++ backend/hermadata/routers/animal.py
@@ -1,18 +1,27 @@
 from fastapi import APIRouter, Depends
 from hermadata.dependancies import get_repository, get_session
 from sqlalchemy.orm import Session
+from hermadata.repositories.animal.animal import SQLAnimalRepository
 
-from hermadata.repositories.animal import AnimalModel, AnimalQueryModel, NewAnimalModel, SQLAnimalRepository
+from hermadata.repositories.animal.models import (
+    AnimalModel,
+    AnimalQueryModel,
+    NewAnimalModel,
+)
 
 router = APIRouter(prefix="/animal")
 
 
 @router.post("")
-def create_new_animal(data: NewAnimalModel, session: Session = Depends(get_session)):
+def create_new_animal(
+    data: NewAnimalModel, session: Session = Depends(get_session)
+):
     repo = SQLAnimalRepository(session)
 
     animal_code = repo.generate_code(
-        race_code=data.race, origin_city_code=data.origin_city_code, finding_date=data.finding_date
+        race_id=data.race_id,
+        origin_city_code=data.origin_city_code,
+        rescue_date=data.rescue_date,
     )
 
     data = {**data.model_dump(), "code": animal_code}
@@ -23,8 +32,21 @@ def create_new_animal(data: NewAnimalModel, session: Session = Depends(get_sessi
     return animal_code
 
 
+@router.get("")
+def get_animal_list():
+    pass
+
+
+@router.get("search")
+def search_animals():
+    pass
+
+
 @router.get("{animal_id}")
-def get_animal(animal_id: int, repo: SQLAnimalRepository = Depends(get_repository(SQLAnimalRepository))):
+def get_animal(
+    animal_id: int,
+    repo: SQLAnimalRepository = Depends(get_repository(SQLAnimalRepository)),
+):
     animal_data = repo.get(AnimalQueryModel(id=animal_id))
 
     return animal_data
