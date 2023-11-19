d4b513cb6c0bde8569a81442e6788dffd5b8765f
Samuele <samuelevf@gmail.com>
2023-11-19 10:56:46 +0100

    format

--- backend/hermadata/database/alembic/import_initial_data.py
+++ backend/hermadata/database/alembic/import_initial_data.py
@@ -8,53 +8,58 @@ from hermadata.database.models import Comune, Provincia, Race
 
 def import_initial_data(stage: str, engine: Engine):
     Session = sessionmaker(engine)
-    initial_data_dir = os.path.join(os.path.dirname(__file__), 'initial_data')
+    initial_data_dir = os.path.join(os.path.dirname(__file__), "initial_data")
     with Session.begin() as s:
-        with open(os.path.join(initial_data_dir, 'province.csv'), 'r') as fp:
+        with open(os.path.join(initial_data_dir, "province.csv"), "r") as fp:
             reader = csv.DictReader(fp, delimiter=";")
             for r in reader:
                 check = s.execute(
-                    select(Provincia.id).where(Provincia.id == r['sigla_provincia'])
+                    select(Provincia.id).where(
+                        Provincia.id == r["sigla_provincia"]
+                    )
                 ).first()
                 if check:
                     continue
                 s.execute(
                     insert(Provincia).values(
-                        id=r['sigla_provincia'], name=r['denominazione_provincia']
+                        id=r["sigla_provincia"],
+                        name=r["denominazione_provincia"],
                     )
                 )
         s.flush()
-        with open(os.path.join(initial_data_dir, 'comuni.csv'), 'r') as fp:
+        with open(os.path.join(initial_data_dir, "comuni.csv"), "r") as fp:
             reader = csv.DictReader(fp, delimiter=";")
             for i, r in enumerate(reader):
-                if i >= 100 and stage == 'dev':
+                if i >= 100 and stage == "dev":
                     break
                 check = s.execute(
-                    select(Comune.id).where(Comune.id == r['codice_belfiore'])
+                    select(Comune.id).where(Comune.id == r["codice_belfiore"])
                 ).first()
                 if check:
                     continue
                 s.execute(
                     insert(Comune).values(
-                        id=r['codice_belfiore'],
-                        name=r['denominazione_ita'],
-                        provincia=r['sigla_provincia'],
+                        id=r["codice_belfiore"],
+                        name=r["denominazione_ita"],
+                        provincia=r["sigla_provincia"],
                     )
                 )
-        with open(os.path.join(initial_data_dir, 'races.csv'), 'r') as fp:
+        with open(os.path.join(initial_data_dir, "races.csv"), "r") as fp:
             reader = csv.DictReader(fp)
             for i, r in enumerate(reader):
-                check = s.execute(select(Race.code).where(Race.code == r['id'])).first()
+                check = s.execute(
+                    select(Race.id).where(Race.id == r["id"])
+                ).first()
                 if check:
                     continue
                 s.execute(
                     insert(Race).values(
-                        code=r['id'],
-                        name=r['name'],
+                        id=r["id"],
+                        name=r["name"],
                     )
                 )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     settings = Settings()
     import_initial_data(settings.stage, create_engine(settings.db.url))
