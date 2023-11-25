e184bb8dfba9301b7bb5e4baea6303ea1b3d2c6b
Samuele <samuelevf@gmail.com>
2023-11-25 19:23:08 +0100

    fe: tailwind colors from css variables

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,10 @@
 module.exports = {
     content: ["./src/**/*.{js,ts,jsx,tsx}"],
     plugins: [],
+    extend: {
+        colors: {
+            "primary-color": "var(--primary-color)",
+            "secondary-color": "var(--secondary-color)",
+        },
+    },
 }
