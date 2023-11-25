2ce9867017f750e8179ff108c7d3e86a9a4ac181
Samuele <samuelevf@gmail.com>
2023-11-25 19:25:03 +0100

    fe: move schemas

--- frontend/src/models/animal.schema.ts
+++ frontend/src/models/animal.schema.ts
@@ -6,13 +6,25 @@ import {
     cityCodeValidator,
 } from "./validators"
 
+export const newAnimalEntrySchema = z.object({
+    race_id: z.string().length(1),
+    rescue_city_code: cityCodeValidator,
+
+    entry_type: z.string(),
+})
+
+export type NewAnimalEntry = z.infer<typeof newAnimalEntrySchema>
+
 export const animalSchema = z.object({
     code: animalCodeValidator,
     name: z.string().optional(),
     race_id: animalRaceValidator,
-    rescue_city_code: cityCodeValidator,
-    rescue_date: z.date(),
-    rescue_type: z.string(),
+    rescue_city: z.string(),
+    rescue_province: z.string(),
+    entry_date: z.date().nullable(),
+    entry_type: z.string(),
+    stage: z.string(),
+    adoptability_index: z.number(),
 })
 
 export type Animal = z.infer<typeof animalSchema>
@@ -24,7 +36,10 @@ export const animalSearchResultSchema = z.object({
     rescue_city_code: cityCodeValidator,
     rescue_city: z.string(),
     rescue_province: z.string().length(2),
-    rescue_date: z.string().transform((str) => new Date(str)),
+    entry_date: z
+        .string()
+        .nullable()
+        .transform((str) => (str && new Date(str)) || null),
 })
 
 export type AnimalSearchResult = z.infer<typeof animalSearchResultSchema>
