26599b2cf8c329982037a965fef7fa6572bc3835
Samuele <samuelevf@gmail.com>
2023-11-25 19:22:42 +0100

    be: add util endpoints

--- backend/hermadata/routers/util_router.py
+++ backend/hermadata/routers/util_router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends
 from hermadata.dependancies import get_session
 from sqlalchemy.orm import Session
+from hermadata.models import UtilElement
 from hermadata.repositories.city_repository import (
     ComuneModel,
     ProvinciaModel,
@@ -25,3 +26,36 @@ def get_comuni(provincia: str, session: Session = Depends(get_session)):
 
     comuni = repo.get_comuni(provincia=provincia)
     return comuni
+
+
+@router.get("/entry-types", response_model=list[UtilElement])
+def get_entry_types():
+    return [
+        {
+            "id": "R",
+            "label": "Recupero",
+        },
+        {
+            "id": "C",
+            "label": "Conferimento",
+        },
+        {
+            "id": "S",
+            "label": "Sequestro",
+        },
+    ]
+
+
+@router.get("/animal-stages", response_model=list[UtilElement])
+def get_animal_stages():
+    return [
+        {
+            "id": "S",
+            "label": "Sanitario",
+        },
+        {
+            "id": "R",
+            "label": "Rifugio",
+        },
+        {"id": "P", "label": "Preaffido"},
+    ]
