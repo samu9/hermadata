a67e0db690e3fb2a11e10ba49511af37d244a5e5
Samuele <samuelevf@gmail.com>
2023-11-27 00:03:09 +0100

    fe: add route

--- frontend/src/router/routes.tsx
+++ frontend/src/router/routes.tsx
@@ -6,6 +6,7 @@ import AnimalProfilePage from "../pages/AnimalProfilePage"
 import AnimalDocs from "../components/animal/AnimalDocs"
 import AnimalEvents from "../components/animal/AnimalEvents"
 import AnimalOverview from "../components/animal/AnimalOverview"
+import AnimalEditForm from "../components/animal/AnimalEditForm"
 
 const routes: RouteObject[] = [
     {
@@ -25,6 +26,7 @@ const routes: RouteObject[] = [
                     { path: "", element: <AnimalOverview /> },
                     { path: "docs", element: <AnimalDocs /> },
                     { path: "events", element: <AnimalEvents /> },
+                    { path: "edit", element: <AnimalEditForm /> },
                 ],
             },
         ],
