628309c1c114d128bde526a8d5bc9321e3860f4c
Samuele <samuelevf@gmail.com>
2023-11-27 00:04:38 +0100

    fe: update overvew component

--- frontend/src/components/animal/AnimalOverview.tsx
+++ frontend/src/components/animal/AnimalOverview.tsx
@@ -1,5 +1,31 @@
+import { faMapPin, faWarning } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { Button } from "primereact/button"
+
 const AnimalOverview = () => {
-    return <div>Panoramica</div>
+    return (
+        <div>
+            <div className="w-full text-center">
+                <Button severity="warning" outlined raised>
+                    <FontAwesomeIcon icon={faWarning} /> Completa ingresso
+                </Button>
+            </div>
+
+            {/* <div>
+                    <FontAwesomeIcon icon={faMapPin} /> {props.data.rescue_city}{" "}
+                    ({props.data.rescue_province})
+                </div>
+                <div>
+                    <div className="text-xs text-gray-700">Ingresso </div>
+
+                    <div className="font-bold">
+                        {(props.data.entry_date &&
+                            format(props.data.entry_date, "dd/MM/y")) ||
+                            "-"}
+                    </div>
+                </div> */}
+        </div>
+    )
 }
 
 export default AnimalOverview
