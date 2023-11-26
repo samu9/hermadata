3d8cc321af44e04f044e83b2ca882eb53205f376
Samuele <samuelevf@gmail.com>
2023-11-27 00:03:33 +0100

    fe: update animal schems

--- frontend/src/models/animal.schema.ts
+++ frontend/src/models/animal.schema.ts
@@ -4,6 +4,7 @@ import {
     animalCodeValidator,
     animalRaceValidator,
     cityCodeValidator,
+    dateOnly,
 } from "./validators"
 
 export const newAnimalEntrySchema = z.object({
@@ -17,22 +18,32 @@ export type NewAnimalEntry = z.infer<typeof newAnimalEntrySchema>
 
 export const animalSchema = z.object({
     code: animalCodeValidator,
-    name: z.string().optional(),
+    name: z.string().nullable().optional(),
     race_id: animalRaceValidator,
     rescue_city: z.string(),
     rescue_province: z.string(),
-    entry_date: z.date().nullable(),
+    entry_date: dateOnly.optional(),
     entry_type: z.string(),
     stage: z.string(),
-    adoptability_index: z.number(),
+    adoptability_index: z.number().optional(),
+    chip_code: z.string().optional(),
 })
 
 export type Animal = z.infer<typeof animalSchema>
+
+export const animalEditSchema = animalSchema.extend({
+    sterilized: z.boolean().nullable(),
+    sex: z.number().nullable(),
+    breed_id: z.number().nullable(),
+})
+
+export type AnimalEdit = z.infer<typeof animalEditSchema>
 
 export const animalSearchResultSchema = z.object({
     code: z.string(), //animalCodeValidator,
     name: z.string().nullable(),
     race_id: animalRaceValidator,
+    chip_code: z.string().optional(),
     rescue_city_code: cityCodeValidator,
     rescue_city: z.string(),
     rescue_province: z.string().length(2),
