ccc92a5c3dbade53877f47bfccc33611093e0936
Samuele <samuelevf@gmail.com>
2023-11-27 23:32:04 +0100

    fe: preprocess date validator if string

--- frontend/src/models/validators.ts
+++ frontend/src/models/validators.ts
@@ -5,10 +5,16 @@ export const animalRaceValidator = z.string().length(1)
 export const cityCodeValidator = z.string().regex(/[A-Z]\d{3}/)
 export const provinceValidator = z.string().length(2)
 
-export const dateOnly = z.date().transform((d) => {
-    // prime-react calendar returns a date at 0 hour at local time.
-    // this results at an UTC date for the previous day at 23 hour.
-    d.setUTCDate(d.getDate())
-    d.setUTCHours(0, 0, 0, 0)
-    return new Date(d)
-})
+export const dateOnly = z
+    .preprocess(
+        (arg) => (typeof arg == "string" ? new Date(arg) : arg),
+        z.date()
+    )
+    .transform((d) => {
+        // prime-react calendar returns a date at 0 hour at local time.
+        // this results at an UTC date for the previous day at 23 hour.
+
+        d.setUTCDate(d.getDate())
+        d.setUTCHours(0, 0, 0, 0)
+        return new Date(d)
+    })
