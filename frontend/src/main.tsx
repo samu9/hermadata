aed532aa01b5a9e96270b3befb684298ed607386
Samuele <samuelevf@gmail.com>
2023-11-27 23:33:45 +0100

    fe: query client config

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,19 +1,20 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import App from "./App.tsx"
 import "./index.css"
 import ApiService from "./services/api.ts"
 import { QueryClient, QueryClientProvider } from "react-query"
 import "primereact/resources/themes/lara-light-indigo/theme.css"
 import { PrimeReactProvider } from "primereact/api"
-import {
-    BrowserRouter,
-    RouterProvider,
-    createBrowserRouter,
-} from "react-router-dom"
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
 import routes from "./router/routes.tsx"
 export const apiService = new ApiService(import.meta.env.VITE_API_BASE_URL)
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus: false, // default: true
+        },
+    },
+})
 export const router = createBrowserRouter(routes)
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
