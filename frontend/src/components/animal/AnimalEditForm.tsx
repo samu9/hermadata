92fed206cb0a35aa7d03d760c2b70e0a980f672c
Samuele <samuelevf@gmail.com>
2023-12-03 10:30:13 +0100

    fe: update edit animal form

--- frontend/src/components/animal/AnimalEditForm.tsx
+++ frontend/src/components/animal/AnimalEditForm.tsx
@@ -1,37 +1,70 @@
 import { zodResolver } from "@hookform/resolvers/zod"
 import { Button } from "primereact/button"
 import { Divider } from "primereact/divider"
-import { useEffect } from "react"
+import { Toast } from "primereact/toast"
+import { useEffect, useRef } from "react"
 import { FormProvider, useForm } from "react-hook-form"
+import { useMutation, useQueryClient } from "react-query"
 import { useLocation, useParams } from "react-router-dom"
 import { apiService } from "../../main"
 import { AnimalEdit, animalEditSchema } from "../../models/animal.schema"
+import { useAnimalQuery } from "../../queries"
+import ControlledBreedsDropdown from "../forms/ControlledBreedsDropdown"
 import ControlledCheckbox from "../forms/ControlledCheckbox"
 import ControlledInputDate from "../forms/ControlledInputDate"
 import ControlledInputText from "../forms/ControlledInputText"
 import ControlledRadio from "../forms/ControlledRadio"
+import ControlledTextarea from "../forms/ControlledTextarea"
 
 const AnimalEditForm = () => {
     const { id } = useParams()
     const location = useLocation()
+    const animalQuery = useAnimalQuery(id!)
     const form = useForm<AnimalEdit>({
         resolver: zodResolver(animalEditSchema),
-        defaultValues: { ...animalEditSchema.parse(location.state) },
+        defaultValues: { ...animalEditSchema.parse(animalQuery.data) },
     })
+    const toast = useRef<Toast>(null)
 
     const {
         formState: { errors },
         handleSubmit,
         watch,
+        setValue,
     } = form
 
+    useEffect(() => {
+        console.log(watch())
+    }, [watch()])
+    const queryClient = useQueryClient()
+
+    const updateAnimalMutation = useMutation({
+        mutationFn: (args: { id: string; data: AnimalEdit }) =>
+            apiService.updateAnimal(args.id, args.data),
+        onSuccess: (
+            result: boolean,
+            variables: { id: string; data: AnimalEdit },
+            context
+        ) => {
+            queryClient.setQueryData(["animal", variables.id], variables.data)
+            toast.current?.show({
+                severity: "success",
+                summary: "Scheda aggiornata",
+            })
+        },
+        onError: () =>
+            toast.current?.show({
+                severity: "error",
+                summary: "Qualcosa è andato storto",
+            }),
+        mutationKey: "updateAnimal",
+    })
     const onSubmit = async (data: AnimalEdit) => {
         if (!id) {
             return false
         }
-        const result = await apiService.updateAnimal(id, data)
-
-        return result
+        await updateAnimalMutation.mutateAsync({ id, data })
+        await apiService.updateAnimal(id, data)
     }
 
     return (
@@ -49,6 +82,7 @@ const AnimalEditForm = () => {
                             <ControlledInputText<AnimalEdit>
                                 fieldName="chip_code"
                                 label="Chip"
+                                disabled={animalQuery.data?.chip_code_set}
                                 className="w-52"
                             />
                             <div className="flex gap-4">
@@ -79,13 +113,27 @@ const AnimalEditForm = () => {
                                         { value: 1, label: "Femmina" },
                                     ]}
                                 />
+                                <ControlledInputDate<AnimalEdit>
+                                    fieldName="birth_date"
+                                    label="Data di nascita"
+                                    className="w-32"
+                                />
                             </div>
+                            <ControlledBreedsDropdown
+                                raceId={location.state.race_id}
+                            />
                         </div>
+                        <ControlledTextarea<AnimalEdit>
+                            fieldName="notes"
+                            label="Note"
+                            className="max-w-md"
+                        />
                     </div>
                     <Divider />
                     <Button type="submit">Salva</Button>
                 </form>
             </FormProvider>
+            <Toast ref={toast} position="bottom-right" />
         </div>
     )
 }
