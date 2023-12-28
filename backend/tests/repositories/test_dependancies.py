dda4b2c134766eb7b9e8a43a440ef02189a8cedd
Samuele <samuelevf@gmail.com>
2023-12-28 12:16:00 +0100

    add test

---
+++ backend/tests/repositories/test_dependancies.py
@@ -0,0 +1,2 @@
+def test_get_document_repository():
+    pass
--- backend/tests/repositories/test_document_repository.py
+++ backend/tests/repositories/test_document_repository.py
@@ -9,6 +9,14 @@ from hermadata.repositories.document_repository import (
 from hermadata.storage.disk_storage import DiskStorage
 
 
+def test_get_kinds(db_session: Session):
+    repo = SQLDocumentRepository(db_session)
+    kinds = repo.get_all_document_kinds()
+
+    assert len(kinds) == 1
+    assert kinds[0].name == "Intervento di recupero"
+
+
 def test_new_document(db_session: Session, disk_storage: DiskStorage):
     repo = SQLDocumentRepository(db_session, {StorageType.disk: disk_storage})
     data = NewDocument(
