7837bf6a98eac49c7388b229457d2045d513357b
Samuele <samuelevf@gmail.com>
2023-12-28 12:15:14 +0100

    be: add doc repository method

--- backend/hermadata/repositories/document_repository.py
+++ backend/hermadata/repositories/document_repository.py
@@ -2,7 +2,7 @@ from enum import Enum
 from uuid import UUID, uuid4
 from pydantic import BaseModel
 
-from sqlalchemy import insert
+from sqlalchemy import insert, select
 from hermadata.database.models import Document, DocumentKind
 from hermadata.repositories import BaseRepository
 from sqlalchemy.orm import Session
@@ -28,6 +28,10 @@ class DocKindModel(BaseModel):
     name: str
 
 
+class NewDocKindModel(BaseModel):
+    name: str
+
+
 class DocumentModel(BaseModel):
     filename: str
 
@@ -40,11 +44,25 @@ class SQLDocumentRepository(BaseRepository):
         self.session = session
         self.storage = storage
 
-    def new_document_kind(self, name: str):
-        result = self.session.execute(insert(DocumentKind).values(name=name))
+    def new_document_kind(self, data: NewDocKindModel):
+        result = self.session.execute(
+            insert(DocumentKind).values(name=data.name)
+        )
         self.session.commit()
-        new_kind = DocKindModel(id=result.lastrowid, name=name)
+        new_kind = DocKindModel(id=result.lastrowid, name=data.name)
         return new_kind
+
+    def get_all_document_kinds(self):
+        select_result = (
+            self.session.execute(select(DocumentKind)).scalars().all()
+        )
+
+        result = [
+            DocKindModel.model_validate(r, from_attributes=True)
+            for r in select_result
+        ]
+
+        return result
 
     def new_document(self, data: NewDocument) -> UUID:
         key = str(uuid4())
