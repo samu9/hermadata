b215eeb49ac73debf038dc703b033f9ea9f56d4a
Samuele <samuelevf@gmail.com>
2023-12-28 12:07:59 +0100

    be: restructure dependancies using fastapi di system

--- backend/hermadata/dependancies.py
+++ backend/hermadata/dependancies.py
@@ -1,35 +1,38 @@
 from ast import TypeVar
-from functools import lru_cache
-from typing import Callable, Type
+from typing import Annotated, Any, Callable, Type
+from fastapi import Depends
 
-from sqlalchemy import create_engine
+from sqlalchemy import Engine, create_engine
 from sqlalchemy.orm import sessionmaker
+from hermadata.repositories.animal.animal_repository import SQLAnimalRepository
+from hermadata.repositories.document_repository import (
+    SQLDocumentRepository,
+    StorageType,
+)
 from hermadata.settings import Settings
+from hermadata.storage.disk_storage import DiskStorage
+from sqlalchemy.orm import Session
 
 
-@lru_cache()
 def get_settings():
     settings = Settings()
     return settings
 
 
-@lru_cache()
-def get_engine():
+def get_engine(
+    settings: Annotated[Settings, Depends(get_settings, use_cache=True)]
+):
     settings = get_settings()
     engine = create_engine(settings.db.url)
     return engine
 
 
-def get_session(readwrite=False):
-    session = sessionmaker(get_engine())
+def get_session(engine: Annotated[Engine, Depends(get_engine)]):
+    session = sessionmaker(engine)
 
     try:
-        if readwrite:
-            with session() as s:
-                yield s
-        else:
-            with session.begin() as s:
-                yield s
+        with session.begin() as s:
+            yield s
     except Exception as e:
         s.rollback()
         raise e
@@ -38,7 +41,55 @@ def get_session(readwrite=False):
 Repo = TypeVar("T")
 
 
-def get_repository(class_name: Type[Repo], readwrite=False) -> Callable[[], Repo]:
-    session = get_session(readwrite)
+def get_disk_storage(
+    settings: Annotated[Settings, Depends(get_settings, use_cache=True)]
+):
+    disk_storage = DiskStorage(settings.storage.disk.base_path)
 
-    return lambda: class_name(session)
+    return disk_storage
+
+
+def document_repository_factory(
+    db_session: Annotated[Session, Depends(get_session)],
+    disk_storage: Annotated[
+        DiskStorage, Depends(get_disk_storage, use_cache=True)
+    ],
+):
+    db_session_generator = get_session()
+    storage = {StorageType.disk: disk_storage}
+    with db_session_generator as db_session:
+        repo = SQLDocumentRepository(db_session, storage=storage)
+
+        yield repo
+
+
+def animal_repository_factory(
+    db_session: Annotated[Session, Depends(get_session)]
+):
+    repo = SQLAnimalRepository(db_session)
+    yield repo
+
+
+REPOSITORIES_FACTORIES = {SQLAnimalRepository: document_repository_factory}
+
+
+class RepositoryFactory:
+    def __init__(self, repo_class: Type[Repo]) -> None:
+        self.repo_class = repo_class
+
+    def __call__(
+        self, db_session: Annotated[Session, Depends(get_session)]
+    ) -> Any:
+        yield self.repo_class(db_session)
+
+
+def get_repository(
+    class_name: Type[Repo], readwrite=False
+) -> Callable[[], Repo]:
+    db_session_generator = get_session(readwrite)
+
+    def factory():
+        with db_session_generator as db_session:
+            yield class_name(db_session)
+
+    return factory
