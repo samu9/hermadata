03aa444a14463fa41e349638ce3ad99811431780
Samuele <samuelevf@gmail.com>
2023-12-28 15:11:21 +0100

    be: rebuild db migration

--- backend/hermadata/database/alembic/versions/d3c24e367230_initial_tables.py
+++ backend/hermadata/database/alembic/versions/d3c24e367230_initial_tables.py
@@ -1,8 +1,8 @@
 """initial tables
 
-Revision ID: e6aca464c727
+Revision ID: d3c24e367230
 Revises: 1bb12e9dcab1
-Create Date: 2023-11-25 19:54:23.197966
+Create Date: 2023-12-28 15:07:27.037511
 
 """
 from typing import Sequence, Union
@@ -14,7 +14,7 @@ from hermadata.database.alembic.import_initial_data import import_doc_kinds
 
 
 # revision identifiers, used by Alembic.
-revision: str = "e6aca464c727"
+revision: str = "d3c24e367230"
 down_revision: Union[str, None] = "1bb12e9dcab1"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
@@ -44,6 +44,22 @@ def upgrade() -> None:
         sa.PrimaryKeyConstraint("id"),
     )
     op.create_table(
+        "document",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("storage_service", sa.String(length=2), nullable=False),
+        sa.Column("key", sa.String(length=40), nullable=False),
+        sa.Column("filename", sa.String(length=100), nullable=False),
+        sa.Column("mimetype", sa.String(length=50), nullable=False),
+        sa.Column(
+            "created_at",
+            sa.DateTime(),
+            server_default=sa.text("now()"),
+            nullable=False,
+        ),
+        sa.Column("updated_at", sa.DateTime(), nullable=True),
+        sa.PrimaryKeyConstraint("id"),
+    )
+    op.create_table(
         "document_kind",
         sa.Column("name", sa.String(length=50), nullable=False),
         sa.Column("id", sa.Integer(), nullable=False),
@@ -55,7 +71,7 @@ def upgrade() -> None:
         ),
         sa.Column("updated_at", sa.DateTime(), nullable=True),
         sa.PrimaryKeyConstraint("id"),
-        sa.UniqueConstraint("name", name="unique_name"),
+        sa.UniqueConstraint("name"),
     )
     op.create_table(
         "procedure_kind",
@@ -114,27 +130,6 @@ def upgrade() -> None:
         sa.UniqueConstraint("name", "race_id", name="unique_breed"),
     )
     op.create_table(
-        "document",
-        sa.Column("id", sa.Integer(), nullable=False),
-        sa.Column("kind_id", sa.Integer(), nullable=False),
-        sa.Column(
-            "created_at",
-            sa.DateTime(),
-            server_default=sa.text("now()"),
-            nullable=False,
-        ),
-        sa.Column("key", sa.String(length=40), nullable=False),
-        sa.Column("filename", sa.String(length=100), nullable=False),
-        sa.Column("mimetype", sa.String(length=50), nullable=False),
-        sa.Column("storage_service", sa.String(length=2), nullable=False),
-        sa.Column("updated_at", sa.DateTime(), nullable=True),
-        sa.ForeignKeyConstraint(
-            ["kind_id"],
-            ["document_kind.id"],
-        ),
-        sa.PrimaryKeyConstraint("id"),
-    )
-    op.create_table(
         "procedure",
         sa.Column("id", sa.Integer(), nullable=False),
         sa.Column("kind_id", sa.Integer(), nullable=False),
@@ -188,16 +183,16 @@ def upgrade() -> None:
         sa.Column("stage", sa.String(length=1), nullable=True),
         sa.Column("name", sa.String(length=100), nullable=True),
         sa.Column("chip_code", sa.String(length=100), nullable=True),
+        sa.Column(
+            "chip_code_set",
+            sa.Boolean(),
+            server_default=sa.text("false"),
+            nullable=False,
+        ),
         sa.Column("breed_id", sa.Integer(), nullable=True),
         sa.Column("sex", sa.Integer(), nullable=True),
         sa.Column("birth_date", sa.Date(), nullable=True),
         sa.Column("sterilized", sa.Boolean(), nullable=True),
-        sa.Column(
-            "chip_code_set",
-            sa.Boolean(),
-            nullable=True,
-            server_default=sa.text("false"),
-        ),
         sa.Column("adoptable", sa.Boolean(), nullable=True),
         sa.Column("adoptability_index", sa.Integer(), nullable=True),
         sa.Column("color", sa.String(length=100), nullable=True),
@@ -267,6 +262,39 @@ def upgrade() -> None:
         sa.PrimaryKeyConstraint("id"),
     )
     op.create_table(
+        "animal_document",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("animal_id", sa.Integer(), nullable=False),
+        sa.Column("document_id", sa.Integer(), nullable=False),
+        sa.Column("document_kind_id", sa.Integer(), nullable=True),
+        sa.Column(
+            "created_at",
+            sa.DateTime(),
+            server_default=sa.text("now()"),
+            nullable=False,
+        ),
+        sa.Column("updated_at", sa.DateTime(), nullable=True),
+        sa.ForeignKeyConstraint(
+            ["animal_id"],
+            ["animal.id"],
+        ),
+        sa.ForeignKeyConstraint(
+            ["document_id"],
+            ["document.id"],
+        ),
+        sa.ForeignKeyConstraint(
+            ["document_kind_id"],
+            ["document_kind.id"],
+        ),
+        sa.PrimaryKeyConstraint("id"),
+        sa.UniqueConstraint(
+            "animal_id",
+            "document_id",
+            "document_kind_id",
+            name="unique_animal_document",
+        ),
+    )
+    op.create_table(
         "animal_log",
         sa.Column("id", sa.Integer(), nullable=False),
         sa.Column("animal_id", sa.Integer(), nullable=False),
@@ -324,16 +352,17 @@ def downgrade() -> None:
     # ### commands auto generated by Alembic - please adjust! ###
     op.drop_table("terapy")
     op.drop_table("animal_log")
+    op.drop_table("animal_document")
     op.drop_table("adoption")
     op.drop_table("procedure_document_checklist")
     op.drop_table("animal")
     op.drop_table("procedure_document")
     op.drop_table("procedure")
-    op.drop_table("document")
     op.drop_table("breed")
     op.drop_table("vet")
     op.drop_table("users")
     op.drop_table("procedure_kind")
     op.drop_table("document_kind")
+    op.drop_table("document")
     op.drop_table("adopter")
     # ### end Alembic commands ###
