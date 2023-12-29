bf2536e4ed27d63bba2b0949561bce161152e3db
Samuele <samuelevf@gmail.com>
2023-12-29 12:41:12 +0100

    fe: add validators

--- frontend/src/models/validators.ts
+++ frontend/src/models/validators.ts
@@ -10,16 +10,19 @@ export const breedNameValidator = z
     })
     .regex(/^(.*[a-zA-Z]){2,}.*$/)
 
-export const dateOnly = z
-    .preprocess(
-        (arg) => (typeof arg == "string" ? new Date(arg) : arg),
-        z.date()
-    )
-    .transform((d) => {
-        // prime-react calendar returns a date at 0 hour at local time.
-        // this results at an UTC date for the previous day at 23 hour.
+export const dateFromString = z.preprocess(
+    (arg) => (typeof arg == "string" ? new Date(arg) : arg),
+    z.date()
+)
+export const dateOnly = dateFromString.transform((d) => {
+    // prime-react calendar returns a date at 0 hour at local time.
+    // this results at an UTC date for the previous day at 23 hour.
 
-        d.setUTCDate(d.getDate())
-        d.setUTCHours(0, 0, 0, 0)
-        return new Date(d)
-    })
+    d.setUTCDate(d.getDate())
+    d.setUTCHours(0, 0, 0, 0)
+    return new Date(d)
+})
+
+export const docKindNameValidator = z
+    .string()
+    .min(3, "Il nome deve avere almeno 3 caratteri.")
