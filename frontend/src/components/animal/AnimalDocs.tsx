f15ae536750fa10780362b466dbf99ca85716a47
Samuele <samuelevf@gmail.com>
2023-12-29 12:37:38 +0100

    fe: update animal docs component

--- frontend/src/components/animal/AnimalDocs.tsx
+++ frontend/src/components/animal/AnimalDocs.tsx
@@ -4,28 +4,48 @@ import { format } from "date-fns"
 import { Column } from "primereact/column"
 import { DataTable } from "primereact/datatable"
 import { useParams } from "react-router-dom"
+import { useAnimalDocumentsQuery, useDocKindsQuery } from "../../queries"
+import { DocKind } from "../../models/docs.schema"
+import { apiService } from "../../main"
+import NewAnimalDocument from "../new-animal-document/NewDocument"
 
 const AnimalDocs = () => {
-    const { code } = useParams()
+    const { id } = useParams()
+
+    const docKindsQuery = useDocKindsQuery()
+
+    const docKindsMap =
+        docKindsQuery.data?.reduce(
+            (result: { [key: number]: string }, current: DocKind) => {
+                result[current.id] = current.name
+                return result
+            },
+            {}
+        ) || {}
+
+    const animalDocumentsQuery = useAnimalDocumentsQuery(parseInt(id!))
 
     return (
         <div>
             <DataTable
                 selectionMode="single"
-                value={[
-                    {
-                        id: 1,
-                        name: "Intervento recupero",
-                        date: new Date("2023-11-11"),
-                    },
-                ]}
+                onSelectionChange={(e) => apiService.openDocument(e.value.id)}
+                value={
+                    animalDocumentsQuery.data?.map((d) => ({
+                        id: d.document_id,
+                        kind: docKindsMap[d.document_kind_id],
+                        created_at: d.created_at,
+                    })) || []
+                }
                 showHeaders={false}
                 dataKey="id"
             >
-                <Column field="name" style={{ width: "100%" }} />
-                <Column body={(data) => format(data.date, "dd/MM/y")} />
+                <Column field="kind" style={{ width: "100%" }} />
+                <Column body={(data) => format(data.created_at, "dd/MM/y")} />
                 <Column body={() => <FontAwesomeIcon icon={faFilePdf} />} />
             </DataTable>
+
+            <NewAnimalDocument />
         </div>
     )
 }
