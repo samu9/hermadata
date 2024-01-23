192f5a0277bbfb24214da7f9d0300d66fe9b71fb
Samuele <samuelevf@gmail.com>
2024-01-23 22:05:51 +0100

    fe: add search adopter form

---
+++ frontend/src/components/adoption/SearchAdopter.tsx
@@ -0,0 +1,73 @@
+import { useQuery } from "react-query"
+import UncontrolledInputText from "../forms/uncontrolled/UncontrolledInputText"
+import { useState } from "react"
+import { apiService } from "../../main"
+import { Button } from "primereact/button"
+import { Adopter } from "../../models/adopter.schema"
+import AdopterCard from "./AdopterCard"
+
+type ResultProps = {
+    data: Adopter
+    onSelected: (adopter: Adopter) => void
+}
+const SearchResult = (props: ResultProps) => {
+    return (
+        <div className="flex gap-4 w-full items-center">
+            <div className="grow">
+                <AdopterCard data={props.data} />
+            </div>
+            <Button
+                severity="success"
+                onClick={() => props.onSelected(props.data)}
+            >
+                Seleziona
+            </Button>
+        </div>
+    )
+}
+
+type SearchAdopterProps = {
+    onNoResultsCallback: () => void
+    onSelected: (adopter: Adopter) => void
+}
+const SearchAdopter = (props: SearchAdopterProps) => {
+    const [fiscalCode, setFiscalCode] = useState<string | null>(null)
+    const [results, setResults] = useState<Adopter[]>([])
+    const searchQuery = useQuery(["adopter", fiscalCode], {
+        enabled: false,
+        queryFn: () => apiService.searchAdopter(fiscalCode!),
+        onSuccess: (results) => {
+            setResults(results)
+            if (results.length == 0) {
+                props.onNoResultsCallback()
+            }
+        },
+    })
+    return (
+        <div>
+            <div className="flex gap-1 items-end">
+                <UncontrolledInputText
+                    className="w-40"
+                    label="Codice fiscale"
+                    onChange={(v) => setFiscalCode(v)}
+                />
+                <Button className="py-2" onClick={() => searchQuery.refetch()}>
+                    Cerca
+                </Button>
+            </div>
+            <div className="flex gap-2 my-4 flex-col">
+                {results.map((r) => (
+                    <SearchResult
+                        data={r}
+                        onSelected={() => {
+                            setResults([])
+                            props.onSelected(r)
+                        }}
+                    />
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default SearchAdopter
