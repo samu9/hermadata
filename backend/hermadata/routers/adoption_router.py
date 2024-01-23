2cf2d9bde0371993f17a3b74ca674b3ffeb09e32
Samuele <samuelevf@gmail.com>
2024-01-23 22:01:08 +0100

    be: add adoption router

---
+++ backend/hermadata/routers/adoption_router.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter, Depends, HTTPException
+from hermadata.dependancies import RepositoryFactory
+from hermadata.repositories.adoption_repository import (
+    AdoptionModel,
+    ExistingAdoptionException,
+    NewAdoption,
+    SQLAdopionRepository,
+)
+
+
+router = APIRouter(prefix="/adoption")
+
+
+@router.post("", response_model=AdoptionModel)
+def new_animal_adoption(
+    data: NewAdoption,
+    repo: SQLAdopionRepository = Depends(
+        RepositoryFactory(SQLAdopionRepository)
+    ),
+):
+    try:
+        result = repo.create(data)
+    except ExistingAdoptionException:
+        raise HTTPException(status_code=400, detail={})
+
+    return result
