826ebcd3fe4b271aaab275e8da61f0fa71c0d1ef
Samuele <samuelevf@gmail.com>
2024-01-23 22:04:22 +0100

    be: add search adopter method

--- backend/hermadata/repositories/adopter_repository.py
+++ backend/hermadata/repositories/adopter_repository.py
@@ -1,6 +1,6 @@
 from datetime import date
 from pydantic import BaseModel
-from sqlalchemy import insert
+from sqlalchemy import insert, select
 from hermadata.database.models import Adopter
 from hermadata.repositories import BaseRepository
 from sqlalchemy.orm import Session
@@ -20,6 +20,12 @@ class AdopterModel(NewAdopter):
     id: int
 
 
+class AdopterQuery(BaseModel):
+    name: str | None = None
+    surname: str | None = None
+    fiscal_code: str | None = None
+
+
 class SQLAdopterRepository(BaseRepository):
     def __init__(self, session: Session) -> None:
         self.session = session
@@ -33,3 +39,18 @@ class SQLAdopterRepository(BaseRepository):
         adopter_id = result.lastrowid
 
         return AdopterModel(**dump, id=adopter_id)
+
+    def search(self, query: AdopterQuery) -> list[AdopterModel]:
+        where = []
+        if query.fiscal_code is not None:
+            where.append(Adopter.fiscal_code == query.fiscal_code)
+
+        result = (
+            self.session.execute(select(Adopter).where(*where)).scalars().all()
+        )
+
+        adopters = [
+            AdopterModel.model_validate(r, from_attributes=True) for r in result
+        ]
+
+        return adopters
