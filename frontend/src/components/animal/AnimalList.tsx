1502db1824a16431a629186fb7eadfe6d90c6211
Samuele <samuelevf@gmail.com>
2024-02-11 15:51:54 +0100

    fe: improved animal list

--- frontend/src/components/animal/AnimalList.tsx
+++ frontend/src/components/animal/AnimalList.tsx
@@ -1,26 +1,33 @@
-import { useEffect, useState } from "react"
-import { useQuery } from "react-query"
-import { apiService } from "../../main"
+import { IconDefinition, faCat, faDog } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { format } from "date-fns"
+import { FilterMatchMode } from "primereact/api"
+import { Column, ColumnFilterElementTemplateOptions } from "primereact/column"
 import {
     DataTable,
     DataTableFilterMeta,
     DataTableFilterMetaData,
+    DataTableSortEvent,
     DataTableStateEvent,
+    SortOrder,
 } from "primereact/datatable"
-import { Column, ColumnFilterElementTemplateOptions } from "primereact/column"
+import { Dropdown } from "primereact/dropdown"
+import { InputSwitch, InputSwitchChangeEvent } from "primereact/inputswitch"
+import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
 import {
     AnimalSearchQuery,
     AnimalSearchResult,
 } from "../../models/animal.schema"
-import { format } from "date-fns"
-import { useNavigate } from "react-router-dom"
-import { FilterMatchMode } from "primereact/api"
+import {
+    useAnimalSearchQuery,
+    useEntryTypesQuery,
+    useExitTypesQuery,
+} from "../../queries"
 import UncontrolledComuniDropdown from "../forms/uncontrolled/UncontrolledComuniDropdown"
-import { useAnimalSearchQuery, useEntryTypesQuery } from "../../queries"
-import { Dropdown } from "primereact/dropdown"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { IconDefinition, faCat, faDog } from "@fortawesome/free-solid-svg-icons"
 import UncontrolledProvinceDropdown from "../forms/uncontrolled/UncontrolledProvinceDropdown"
+import { classNames } from "primereact/utils"
+import { InputText } from "primereact/inputtext"
 
 type LazyTableState = {
     first: number
@@ -36,6 +43,25 @@ const RACE_ICON_MAP: { [key: string]: IconDefinition } = {
     C: faDog,
 }
 
+type SwitchFilterProps = {
+    label: string
+    checked: boolean
+    onChange: (e: InputSwitchChangeEvent) => void
+}
+
+const SwitchFilter = (props: SwitchFilterProps) => {
+    return (
+        <div className="inline-flex text-xs text-slate-600 items-center gap-2 py-1 pl-3 pr-1  border border-slate-200 rounded-full">
+            <label htmlFor="present">{props.label}</label>
+            <InputSwitch
+                id="present"
+                checked={props.checked}
+                onChange={props.onChange}
+            />
+        </div>
+    )
+}
+
 const AnimalList = () => {
     const [totalRecords, setTotalRecords] = useState(0)
     const [provinciaProvenienzaFilter, setProvinciaProvenienzaFilter] =
@@ -45,6 +71,14 @@ const AnimalList = () => {
         rows: 10,
         page: 1,
         filters: {
+            chip_code: {
+                matchMode: FilterMatchMode.EQUALS,
+                value: null,
+            },
+            name: {
+                matchMode: FilterMatchMode.EQUALS,
+                value: null,
+            },
             comune: {
                 matchMode: FilterMatchMode.EQUALS,
                 value: null,
@@ -53,17 +87,30 @@ const AnimalList = () => {
                 matchMode: FilterMatchMode.EQUALS,
                 value: null,
             },
+            exit_type: {
+                matchMode: FilterMatchMode.EQUALS,
+                value: null,
+            },
         },
     })
     const [queryData, setQueryData] = useState<AnimalSearchQuery>({
         from_index: lazyState.first,
         to_index: lazyState.first + lazyState.rows,
-        entry_type: undefined,
-        rescue_city_code: undefined,
+        present: true,
+        not_present: false,
     })
     const animalQuery = useAnimalSearchQuery(queryData)
     const entryTypesQuery = useEntryTypesQuery()
+    const exitTypesQuery = useExitTypesQuery()
     const entryTypesMap = entryTypesQuery.data?.reduce(
+        (result: { [key: string]: string }, current) => {
+            result[current.id] = current.label
+            return result
+        },
+        {}
+    )
+
+    const exitTypesMap = exitTypesQuery.data?.reduce(
         (result: { [key: string]: string }, current) => {
             result[current.id] = current.label
             return result
@@ -96,37 +143,79 @@ const AnimalList = () => {
         )
     }
 
-    const entryTypeFilterTemplate = (
-        options: ColumnFilterElementTemplateOptions
+    const selectFilterTemplate = (
+        options: { id: string; label: string }[],
+        templateOptions: ColumnFilterElementTemplateOptions
     ) => {
         return (
             <Dropdown
-                options={entryTypesQuery.data}
+                options={options}
                 optionLabel="label"
                 optionValue="id"
-                value={options.value}
-                onChange={(e) => options.filterCallback(e.value)}
+                value={templateOptions.value}
+                onChange={(e) => templateOptions.filterCallback(e.value)}
             />
         )
     }
+
+    const textFilterTemplate = (
+        templateOptions: ColumnFilterElementTemplateOptions
+    ) => (
+        <InputText
+            value={templateOptions.value || ""}
+            type="text"
+            onChange={(e) => templateOptions.filterCallback(e.target.value)}
+        />
+    )
     const onFilter = (event: DataTableStateEvent) => {
-        console.log(event)
         setLazyState(event)
+    }
+    const onSort = (event: DataTableSortEvent) => {
+        console.log(event)
+        setLazyState({ ...lazyState, ...event })
     }
     useEffect(() => {
         setQueryData({
+            ...queryData,
             from_index: lazyState.first,
             to_index: lazyState.first + lazyState.rows,
+            sort_field: lazyState.sortField,
+            sort_order: lazyState.sortOrder,
             rescue_city_code: (
                 lazyState.filters["comune"] as DataTableFilterMetaData
             ).value,
             entry_type: (
                 lazyState.filters["entry_type"] as DataTableFilterMetaData
             ).value,
+            exit_type: (
+                lazyState.filters["exit_type"] as DataTableFilterMetaData
+            ).value,
+            chip_code: (
+                lazyState.filters["chip_code"] as DataTableFilterMetaData
+            ).value,
+            name: (lazyState.filters["name"] as DataTableFilterMetaData).value,
         })
     }, [lazyState])
     return (
         <div className="w-full">
+            <div className="py-2 flex gap-2">
+                <SwitchFilter
+                    label="Mostra presenti"
+                    checked={queryData.present || false}
+                    onChange={(e) =>
+                        setQueryData({ ...queryData, present: e.value })
+                    }
+                />
+
+                <SwitchFilter
+                    label="Mostra non presenti"
+                    checked={queryData.not_present || false}
+                    onChange={(e) =>
+                        setQueryData({ ...queryData, not_present: e.value })
+                    }
+                />
+            </div>
+
             <DataTable
                 className="w-full"
                 filters={lazyState.filters}
@@ -140,28 +229,42 @@ const AnimalList = () => {
                 rowsPerPageOptions={[5, 10, 25, 50]}
                 onFilter={onFilter}
                 onPage={setLazyState}
+                onSort={onSort}
+                sortField={lazyState.sortField}
+                sortOrder={lazyState.sortOrder as SortOrder}
                 totalRecords={totalRecords}
                 lazy
+                rowClassName={(rowData) =>
+                    classNames({
+                        "bg-slate-100":
+                            rowData.exit_date && rowData.exit_date < new Date(),
+                    })
+                }
             >
-                {/* <Column
-                    body={(animal: AnimalSearchResult) => (
-                        <img
-                            className="rounded-full h-full"
-                            src="https://www.bil-jac.com/Images/DogPlaceholder.svg"
-                        />
-                    )}
-                /> */}
                 <Column
                     body={(animal: AnimalSearchResult) => (
                         <FontAwesomeIcon
-                            className="text-lg"
+                            className="text-lg  "
                             icon={RACE_ICON_MAP[animal.race_id]}
                         />
                     )}
                 />
-                {/* <Column field="code" header="Codice" /> */}
-                <Column field="name" header="Nome" />
-                <Column field="chip_code" header="Chip" />
+                <Column
+                    field="name"
+                    header="Nome"
+                    filter
+                    showFilterMatchModes={false}
+                    filterElement={textFilterTemplate}
+                    filterField="name"
+                />
+                <Column
+                    filter
+                    showFilterMatchModes={false}
+                    filterElement={textFilterTemplate}
+                    filterField="chip_code"
+                    field="chip_code"
+                    header="Chip"
+                />
                 <Column
                     className="max-w-10 whitespace-nowrap"
                     filter
@@ -182,9 +285,23 @@ const AnimalList = () => {
                     }
                 />
                 <Column
+                    body={(animal: AnimalSearchResult) =>
+                        animal.entry_date &&
+                        format(animal.entry_date, "dd/MM/y")
+                    }
+                    sortable
+                    sortField="entry_date"
+                    header="Data ingresso"
+                />
+                <Column
                     filter
                     showFilterMatchModes={false}
-                    filterElement={entryTypeFilterTemplate}
+                    filterElement={(templateOptions) =>
+                        selectFilterTemplate(
+                            entryTypesQuery.data || [],
+                            templateOptions
+                        )
+                    }
                     filterField="entry_type"
                     body={(animal: AnimalSearchResult) => (
                         <span className="rounded-full text-xs py-1 bg-slate-600 text-white px-2">
@@ -192,14 +309,41 @@ const AnimalList = () => {
                         </span>
                     )}
                     header="Tipo ingresso"
+                    className="text-center"
                 />
-                <Column
-                    body={(animal: AnimalSearchResult) =>
-                        animal.entry_date &&
-                        format(animal.entry_date, "dd/MM/y")
-                    }
-                    header="Data ingresso"
-                />
+
+                {queryData.not_present && (
+                    <Column
+                        body={(animal: AnimalSearchResult) =>
+                            animal.exit_date &&
+                            format(animal.exit_date, "dd/MM/y")
+                        }
+                        header="Data uscita"
+                    />
+                )}
+                {queryData.not_present && (
+                    <Column
+                        filter
+                        showFilterMatchModes={false}
+                        filterElement={(templateOptions) =>
+                            selectFilterTemplate(
+                                exitTypesQuery.data || [],
+                                templateOptions
+                            )
+                        }
+                        filterField="exit_type"
+                        body={(animal: AnimalSearchResult) =>
+                            animal.exit_type && (
+                                <span className="rounded-full text-xs py-1 bg-slate-600 text-white px-2">
+                                    {exitTypesMap?.[animal.exit_type]}
+                                </span>
+                            )
+                        }
+                        header="Tipo uscita"
+                        className="text-center"
+                    />
+                )}
+                <Column header="Stato" />
             </DataTable>
         </div>
     )
