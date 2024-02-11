1bab9415b2935fb45c724d5044cdcb0a1364f7fd
Samuele <samuelevf@gmail.com>
2024-02-11 23:07:55 +0100

    be: add adoption repository method

--- backend/hermadata/repositories/adoption_repository.py
+++ backend/hermadata/repositories/adoption_repository.py
@@ -1,6 +1,7 @@
-import datetime
+from datetime import datetime
 from pydantic import BaseModel
-from sqlalchemy import select
+from sqlalchemy import select, update
+from hermadata.constants import ExitType
 from hermadata.database.models import Adoption, Animal
 from hermadata.repositories import BaseRepository
 from sqlalchemy.orm import Session
@@ -30,6 +31,7 @@ class SQLAdopionRepository(BaseRepository):
         animal_entry_date = self.session.execute(
             select(Animal.entry_date).where(Animal.id == data.animal_id)
         ).one()
+
         existing_adoption = self.session.execute(
             select(Adoption.id).where(Adoption.animal_id == data.animal_id)
         ).first()
@@ -40,11 +42,37 @@ class SQLAdopionRepository(BaseRepository):
         adoption = Adoption(
             animal_id=data.animal_id,
             adopter_id=data.adopter_id,
-            completed_at=datetime.now() if data.completed else None,
+            completed_at=datetime.utcnow() if data.completed else None,
         )
         self.session.add(adoption)
         self.session.flush()
         result = AdoptionModel.model_validate(adoption, from_attributes=True)
+        if data.completed:
+            self.session.execute(
+                update(Animal)
+                .values(
+                    exit_date=adoption.completed_at, exit_type=ExitType.adoption
+                )
+                .where(Animal.id == data.animal_id)
+            )
         self.session.commit()
 
         return result
+
+    def complete(self, adoption_id: int):
+        adoption = self.session.execute(
+            select(Adoption).where(Adoption.id == adoption_id)
+        ).scalar_one()
+        self.session.execute(
+            update(Adoption)
+            .values(completed_at=datetime.utcnow())
+            .where(Adoption.id == adoption_id)
+        )
+        self.session.execute(
+            update(Animal)
+            .values(
+                exit_date=adoption.completed_at, exit_type=ExitType.adoption
+            )
+            .where(Animal.id == adoption.animal_id)
+        )
+        self.session.commit()
