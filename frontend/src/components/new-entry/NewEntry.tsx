aefd9c78b6dc012e7b76c2d46ddc77249196c18f
Samuele <samuelevf@gmail.com>
2024-02-25 23:11:55 +0100

    fe: new entry form can be on an existing animal

--- frontend/src/components/new-entry/NewEntry.tsx
+++ frontend/src/components/new-entry/NewEntry.tsx
@@ -6,18 +6,24 @@ import { useRef } from "react"
 import NewAnimalForm from "./NewAnimalEntryForm"
 import { Toast } from "primereact/toast"
 
-const NewEntry = () => {
+type Props = {
+    // first entry must also specify race
+    animalId?: string
+}
+
+const NewEntry = (props: Props) => {
     const op = useRef<OverlayPanel>(null)
     const toast = useRef<Toast>(null)
     const onNew = (code: string) => {
-        console.log(code)
         toast.current?.show({
             severity: "success",
             summary: "Nuovo ingresso",
-            detail: `Codice: ${code}`,
+            detail: props.animalId ? null : `Codice: ${code}`,
         })
         op.current?.hide()
     }
+    const label = props.animalId ? "Nuovo ingresso" : "Nuovo animale"
+
     return (
         <div>
             <div className="absolute bottom-4 right-4">
@@ -25,11 +31,15 @@ const NewEntry = () => {
                     className="shadow-lg"
                     onClick={(e) => op.current && op.current.toggle(e)}
                 >
-                    <FontAwesomeIcon icon={faPlus} fixedWidth /> Nuovo ingresso
+                    <FontAwesomeIcon icon={faPlus} fixedWidth /> {label}
                 </Button>
                 <OverlayPanel showCloseIcon ref={op}>
                     <div className="w-[20rem]">
-                        <NewAnimalForm onSuccess={onNew.bind(this)} />
+                        <NewAnimalForm
+                            title={label}
+                            animalId={props.animalId}
+                            onSuccess={onNew.bind(this)}
+                        />
                     </div>
                 </OverlayPanel>
                 <Toast ref={toast} position="bottom-right" />
