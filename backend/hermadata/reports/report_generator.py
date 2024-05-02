0b449cabe474792cdecdf8ad39e5a99cffa2a657
Samuele <samuelevf@gmail.com>
2024-05-02 22:37:45 +0200

    be: update report generator

--- backend/hermadata/reports/report_generator.py
+++ backend/hermadata/reports/report_generator.py
@@ -1,11 +1,15 @@
-from datetime import datetime
+from datetime import date, datetime
 import pdfkit
 from jinja2 import (
     Environment,
     FileSystemLoader,
     select_autoescape,
 )
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, field_validator
+
+
+def transform_date_to_string(raw: date) -> str:
+    return raw.strftime("%d/%m/%Y")
 
 
 class ReportDefaultVariables(BaseModel):
@@ -20,22 +24,48 @@ class ReportAnimalEntryVariables(ReportDefaultVariables):
     city: str
     animal_type: str
     animal_name: str | None = None
+    entry_date: str
+
+    transform_entry_date = field_validator("entry_date", mode="before")(
+        transform_date_to_string
+    )
+
+
+class ReportChipAssignmentVariables(ReportDefaultVariables):
+    title: str = "ASSEGNAMENTO CHIP"
+    animal_name: str | None = None
+    chip_code: str
+    assignment_date: str
+
+    transform_assignment_date = field_validator(
+        "assignment_date", mode="before"
+    )(transform_date_to_string)
 
 
 class ReportGenerator:
     def __init__(self, jinja_env: Environment) -> None:
         self.jinja_env = jinja_env
 
-    def build_animal_entry_report(
-        self, variables: ReportAnimalEntryVariables
+    def _build_template(
+        self, filename: str, variables: ReportDefaultVariables
     ) -> bytes:
-        template = self.jinja_env.get_template("animal_entry.jinja")
+        template = self.jinja_env.get_template(filename)
 
         rendered_html = template.render(**variables.model_dump())
 
         pdf = pdfkit.from_string(input=rendered_html)
 
         return pdf
+
+    def build_animal_entry_report(
+        self, variables: ReportAnimalEntryVariables
+    ) -> bytes:
+        return self._build_template("animal_entry.jinja", variables)
+
+    def build_chip_assignment_report(
+        self, variables: ReportChipAssignmentVariables
+    ) -> bytes:
+        return self._build_template("chip_assignment.jinja", variables)
 
 
 if __name__ == "__main__":
