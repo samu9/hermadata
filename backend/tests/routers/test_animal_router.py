30ffeb61a720534d6b45eb83bf9b1efe45f0d786
Samuele <samuelevf@gmail.com>
2024-05-08 20:31:27 +0200

    be: update test

--- backend/tests/routers/test_animal_router.py
+++ backend/tests/routers/test_animal_router.py
@@ -1,21 +1,31 @@
+from datetime import datetime, timedelta
 from fastapi.testclient import TestClient
 from sqlalchemy import select
 
-from hermadata.constants import EntryType
-from hermadata.database.models import Animal
+from hermadata.constants import DocKindCode, EntryType
+from hermadata.database.models import (
+    Animal,
+    AnimalDocument,
+    AnimalEntry,
+    DocumentKind,
+)
 from hermadata.repositories.animal.models import (
+    CompleteEntryModel,
     NewAnimalModel,
     UpdateAnimalModel,
 )
 from sqlalchemy.orm import Session, sessionmaker
+from fastapi.encoders import jsonable_encoder
 
 
 def test_create_animal(app: TestClient, db_session: Session):
-    request_body = NewAnimalModel(
-        race_id="C",
-        rescue_city_code="H501",
-        entry_type=EntryType.confiscation.value,
-    ).model_dump()
+    request_body = jsonable_encoder(
+        NewAnimalModel(
+            race_id="C",
+            rescue_city_code="H501",
+            entry_type=EntryType.confiscation.value,
+        ).model_dump()
+    )
 
     result = app.post("/animal/", json=request_body)
 
@@ -41,9 +51,11 @@ def test_update_animal(
         )
     )
 
-    update_data = UpdateAnimalModel(
-        name="Test", chip_code="123.123.123.123.123"
-    ).model_dump()
+    update_data = jsonable_encoder(
+        UpdateAnimalModel(
+            name="Test", chip_code="123.123.123.123.123"
+        ).model_dump()
+    )
     result = app.post(f"/animal/{animal_id}", json=update_data)
 
     affected = int(result.content.decode())
@@ -57,3 +69,35 @@ def test_update_animal(
         assert animal.name == "Test"
         assert animal.chip_code == "123.123.123.123.123"
         assert animal.chip_code_set
+
+
+def test_complete_entry(
+    app: TestClient, make_animal, DBSessionMaker: sessionmaker
+):
+    animal_id = make_animal()
+
+    entry_date = (datetime.now() + timedelta(days=1)).date()
+    data = jsonable_encoder(
+        CompleteEntryModel(entry_date=entry_date).model_dump()
+    )
+
+    result = app.post(f"/animal/{animal_id}/entry/complete", json=data)
+
+    assert result.status_code == 200
+    with DBSessionMaker() as s:
+        e: AnimalEntry = s.execute(
+            select(AnimalEntry).where(AnimalEntry.animal_id == animal_id)
+        ).scalar_one()
+
+        assert e.entry_date == entry_date
+
+        doc_kind: AnimalDocument = s.execute(
+            select(DocumentKind.code)
+            .select_from(AnimalDocument)
+            .join(
+                DocumentKind, AnimalDocument.document_kind_id == DocumentKind.id
+            )
+            .where(AnimalDocument.animal_id == animal_id)
+        ).scalar()
+
+        assert doc_kind == DocKindCode.documento_ingresso.value
