34ae3a71960de004abf48a1b1bc7143f3a20f66c
Samuele <samuelevf@gmail.com>
2024-08-17 18:26:46 +0200

    fe: change animal exit form adding adopter info

--- frontend/src/components/animal/AnimalExitForm.tsx
+++ frontend/src/components/animal/AnimalExitForm.tsx
@@ -7,17 +7,20 @@ import {
 import ControlledDropdown from "../forms/ControlledDropdown"
 import ControlledInputDate from "../forms/ControlledInputDate"
 import { Button } from "primereact/button"
-import { useEffect, useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 import { useAnimalQuery, useExitTypesQuery } from "../../queries"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useMutation, useQueryClient } from "react-query"
 import { apiService } from "../../main"
 import { Toast } from "primereact/toast"
 import { useParams } from "react-router-dom"
+import SearchAdopter from "../adoption/SearchAdopter"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faAdd, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons"
+import { SubTitle } from "../typography"
+import NewAdopterForm from "../adopter/NewAdopterForm"
+import { Dialog } from "primereact/dialog"
 
-type Props = {
-    animal_id: number
-}
 const AnimalExitForm = () => {
     const { id } = useParams()
     const animalQuery = useAnimalQuery(id!)
@@ -25,6 +28,12 @@ const AnimalExitForm = () => {
     const queryClient = useQueryClient()
 
     const toast = useRef<Toast>(null)
+
+    const [showAdopterForm, setShowAdopterForm] = useState(false)
+    const [dialogVisibile, setDialogVisible] = useState(false)
+    const [adopterAction, setAdopterAction] = useState<"add" | "search">(
+        "search"
+    )
 
     const exitTypesQuery = useExitTypesQuery()
     const form = useForm<AnimalExit>({
@@ -58,37 +67,85 @@ const AnimalExitForm = () => {
         handleSubmit,
         watch,
         formState: { isValid, errors },
+        getValues,
+        setValue,
+        reset,
     } = form
     const onSubmit = (data: AnimalExit) => {
-        animalExitMutation.mutate(data)
+        animalExitMutation.mutate(data, { onSuccess: () => reset() })
     }
     useEffect(() => {
-        console.log(errors, watch())
+        const values = getValues()
+
+        setShowAdopterForm(["A"].includes(values.exit_type))
     }, [watch()])
     return (
         <div>
-            <FormProvider {...form}>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <div className="flex gap-2 mb-2">
-                        <ControlledInputDate<AnimalExit>
-                            fieldName="exit_date"
-                            label="Data uscita"
-                            disabled={true}
-                            className="w-32"
-                        />
-                        <ControlledDropdown
-                            fieldName="exit_type"
-                            label="Tipo uscita"
-                            optionValue="id"
-                            optionLabel="label"
-                            options={exitTypesQuery.data}
-                        />
+            <div className="flex gap-3">
+                <FormProvider {...form}>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <div className="flex gap-2 mb-2">
+                            <ControlledInputDate<AnimalExit>
+                                fieldName="exit_date"
+                                label="Data uscita"
+                                disabled={true}
+                                className="w-32"
+                            />
+                            <ControlledDropdown
+                                fieldName="exit_type"
+                                label="Tipo uscita"
+                                optionValue="id"
+                                optionLabel="label"
+                                options={exitTypesQuery.data}
+                            />
+                        </div>
+                        <Button disabled={!isValid} type="submit">
+                            Salva
+                        </Button>
+                    </form>
+                </FormProvider>
+                {showAdopterForm && (
+                    <div className="border rounded p-4 shadow w-full">
+                        <SubTitle>Adottante</SubTitle>
+
+                        <div className="flex gap-2 mb-2 items-center w-full justify-center">
+                            <Button
+                                onClick={() => {
+                                    setAdopterAction("search")
+                                }}
+                                disabled={adopterAction == "search"}
+                            >
+                                <FontAwesomeIcon icon={faMagnifyingGlass} />
+                            </Button>
+                            <Button
+                                onClick={() => setDialogVisible(true)}
+                                disabled={adopterAction == "add"}
+                            >
+                                <FontAwesomeIcon icon={faAdd} />
+                            </Button>
+                        </div>
+                        {adopterAction == "add" && <NewAdopterForm />}
+                        {adopterAction == "search" && (
+                            <SearchAdopter
+                                onSelected={(a) =>
+                                    setValue("adopter_id", a.id, {
+                                        shouldDirty: true,
+                                    })
+                                }
+                                onNoResultsCallback={() => null}
+                            />
+                        )}
                     </div>
-                    <Button disabled={!isValid} type="submit">
-                        Salva
-                    </Button>
-                </form>
-            </FormProvider>
+                )}
+            </div>
+            <Dialog
+                visible={dialogVisibile}
+                className="w-2/3"
+                header={"Aggiungi adottante"}
+                onHide={() => setDialogVisible(false)}
+            >
+                <NewAdopterForm />
+            </Dialog>
             <Toast ref={toast} position="bottom-right" />
         </div>
     )
