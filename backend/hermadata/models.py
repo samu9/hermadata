3fe0ecd0ac728d56cbced7f4472ea1614669af1b
Samuele <samuelevf@gmail.com>
2024-08-23 14:27:33 +0200

    be: update pagination models

--- backend/hermadata/models.py
+++ backend/hermadata/models.py
@@ -1,7 +1,8 @@
-from typing import Generic, TypeVar
+from typing import Generic, Iterable, TypeVar
 from pydantic import BaseModel
 
 from hermadata.constants import ApiErrorCode
+from sqlalchemy import or_
 
 
 T = TypeVar("T")
@@ -10,6 +11,31 @@ T = TypeVar("T")
 class PaginationQuery(BaseModel):
     from_index: int | None = None
     to_index: int | None = None
+
+    sort_field: str | None = None
+    sort_order: int | None = None
+
+
+class SearchQuery(PaginationQuery):
+    def as_where_clause(self) -> list:
+        or_elems = []
+        where = []
+        for field in self._where_clause_map.keys():
+            value = getattr(self, field)
+            if value is None:
+                continue
+            builder, in_or = self._where_clause_map[field]
+            attribute = builder(value)
+
+            to_add = or_elems if in_or else where
+            if isinstance(attribute, Iterable):
+                to_add.extend(attribute)
+            else:
+                to_add.append(attribute)
+
+        if or_elems:
+            where.append(or_(*or_elems))
+        return where
 
 
 class PaginationResult(BaseModel, Generic[T]):
