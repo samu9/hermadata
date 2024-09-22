14582bf175228e04ef8fb6afaefa315efe0f08f7
Samuele <samuelevf@gmail.com>
2024-09-22 11:07:14 +0200

    be: add move_documents script

---
+++ backend/scripts/move_documents.py
@@ -0,0 +1,61 @@
+import os
+
+from sqlalchemy import create_engine, select, update
+from sqlalchemy.orm import sessionmaker
+
+from hermadata.database.models import Document
+from hermadata.repositories.document_repository import StorageType
+from hermadata.settings import settings
+from hermadata.dependancies import get_s3_storage
+
+
+def upload_pdfs_to_s3():
+    directory_path = settings.storage.disk.base_path
+    # Create an instance of the S3Storage class
+    s3_storage = get_s3_storage()
+    db_session = sessionmaker(create_engine(**settings.db.model_dump()))
+
+    with db_session() as session:
+        keys = session.execute(
+            select(Document.key, Document.filename).where(
+                Document.storage_service == StorageType.disk.value
+            )
+        ).all()
+
+    # Check if the provided directory path is valid
+    if not os.path.isdir(directory_path):
+        print(f"The provided path '{directory_path}' is not a valid directory.")
+        return
+
+    # Loop through all files in the directory
+    for k, filename in keys:
+        if not os.path.exists(os.path.join(directory_path, k)):
+            print(f"{k} not found")
+            continue
+
+        file_path = os.path.join(directory_path, k)
+
+        # Open the PDF and upload its content to S3
+        try:
+            with open(file_path, "rb") as pdf_file:
+                content = pdf_file.read()
+                s3_storage.store_file(k, content)
+                print(f"Uploaded '{filename}' to S3.")
+        except Exception as e:
+            print(f"Failed to upload '{filename}' to S3: {e}")
+            continue
+        with db_session.begin() as session:
+            session.execute(
+                update(Document)
+                .where(Document.key == k)
+                .values({Document.storage_service: StorageType.aws_s3.value})
+            )
+
+
+if __name__ == "__main__":
+    # Define your directory path and S3 bucket name here
+    directory_path = "attic/file_to_move"
+    bucket_name = "hermadata-documents"
+
+    # Call the function to upload PDFs to S3
+    upload_pdfs_to_s3()
