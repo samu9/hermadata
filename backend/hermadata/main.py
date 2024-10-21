9eb093cbef60ce7ef7bf96870c78a90e274e8fa7
Samuele <samuelevf@gmail.com>
2024-10-21 22:41:28 +0200

    be: add initializations to main

--- backend/hermadata/main.py
+++ backend/hermadata/main.py
@@ -5,6 +5,10 @@ import logging.config
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from hermadata.dependancies import get_session_maker
+from hermadata.settings import settings
+from hermadata.constants import StorageType
+from hermadata.repositories.document_repository import SQLDocumentRepository
 from hermadata.routers import (
     adopter_router,
     animal_router,
@@ -14,6 +18,8 @@ from hermadata.routers import (
     util_router,
     vet_router,
 )
+from hermadata.storage.disk_storage import DiskStorage
+from hermadata.storage.s3_storage import S3Storage
 
 logging.config.dictConfig(json.load(open("hermadata/log-configs.json")))
 
@@ -21,7 +27,24 @@ logger = logging.getLogger(__name__)
 
 
 def build_app():
-    app = FastAPI()
+
+    app = FastAPI(lifespan=lifespan)
+
+    s3_storage = S3Storage(settings.storage.s3.bucket)
+    disk_storage = DiskStorage(settings.storage.disk.base_path)
+
+    storage_map = {
+        StorageType.disk: disk_storage,
+        StorageType.aws_s3: s3_storage,
+    }
+
+    session = get_session_maker()
+    with session() as s:
+        SQLDocumentRepository.factory(
+            session=s,
+            selected_storage=settings.storage.selected,
+            storage=storage_map,
+        )
 
     app.add_middleware(
         CORSMiddleware,
@@ -42,6 +65,11 @@ def build_app():
     logger.info("hermadata set up")
 
     return app
+
+
+def lifespan(app: FastAPI):
+
+    yield
 
 
 app = build_app()
