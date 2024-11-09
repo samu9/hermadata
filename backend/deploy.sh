9baab5ec0cd722b9525aff916157e4aec71425b1
Samuele <samuelevf@gmail.com>
2024-11-09 11:20:42 +0100

    be: update deploy script

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,4 +1,13 @@
+#!/bin/bash
+
 STAGE=$1
+
+# Check if STAGE is provided and is either "test" or "prod"
+if [[ -z "$STAGE" || ( "$STAGE" != "test" && "$STAGE" != "prod" ) ]]; then
+    echo "Error: You must provide a valid stage ('test' or 'prod') as the first argument."
+    exit 1
+fi
+
 TAG=deploy_${STAGE}_$(date +%d-%m-%Y_%H-%M-%S)
 git tag $TAG
 git push origin $TAG
