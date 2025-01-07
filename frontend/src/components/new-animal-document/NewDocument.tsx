c8fd54120d2ee68c48c497e89c5f9c06fb27393c
Samuele <samuelevf@gmail.com>
2025-01-07 21:24:40 +0100

    fe: refactor NewDocument component to use OverlayFormButton for document upload

--- frontend/src/components/new-animal-document/NewDocument.tsx
+++ frontend/src/components/new-animal-document/NewDocument.tsx
@@ -1,41 +1,26 @@
 import { faPlus } from "@fortawesome/free-solid-svg-icons"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { Button } from "primereact/button"
-import { OverlayPanel } from "primereact/overlaypanel"
-import { useRef } from "react"
-import { Toast } from "primereact/toast"
-import AnimalDocUploadForm from "../animal/AnimalDocUploadForm"
 import { AnimalDocument } from "../../models/animal.schema"
+import AnimalDocUploadForm from "../animal/AnimalDocUploadForm"
+import OverlayFormButton from "../OverlayFormButton"
 
 const NewAnimalDocument = () => {
-    const op = useRef<OverlayPanel>(null)
-    const toast = useRef<Toast>(null)
-    const onNew = (doc: AnimalDocument) => {
+    const handleNewAnimalDocument = (doc: AnimalDocument) => {
+        console.log("New Animal Document Added:", doc)
+        // Example: Show a toast or update state
         // toast.current?.show({
         //     severity: "success",
         //     summary: "Nuovo ingresso",
-        //     detail: `Codice: ${code}`,
-        // })
-        op.current?.hide()
+        //     detail: `Codice: ${doc.code}`,
+        // });
     }
+
     return (
-        <div>
-            <div className="absolute bottom-4 right-4">
-                <Button
-                    className="shadow-lg"
-                    onClick={(e) => op.current && op.current.toggle(e)}
-                >
-                    <FontAwesomeIcon icon={faPlus} fixedWidth /> Inserisci
-                    documento
-                </Button>
-                <OverlayPanel showCloseIcon ref={op}>
-                    <div className="w-[20rem]">
-                        <AnimalDocUploadForm onSuccess={onNew.bind(this)} />
-                    </div>
-                </OverlayPanel>
-                <Toast ref={toast} position="bottom-right" />
-            </div>
-        </div>
+        <OverlayFormButton
+            buttonText="Inserisci documento"
+            buttonIcon={faPlus}
+            FormComponent={AnimalDocUploadForm}
+            onSuccessAction={handleNewAnimalDocument}
+        />
     )
 }
 
