dd5416236a2bf1ee1d9f4cf4fb7dc4afcc64f45a
Samuele <samuelevf@gmail.com>
2025-01-07 21:23:19 +0100

    fe: integrate Toolbar component with context in AnimalProfilePage

--- frontend/src/pages/AnimalProfilePage.tsx
+++ frontend/src/pages/AnimalProfilePage.tsx
@@ -1,21 +1,24 @@
 import { useParams } from "react-router-dom"
 import AnimalRecord from "../components/animal/AnimalRecord"
+import Toolbar from "../components/Toolbar"
+import { ToolbarProvider } from "../contexts/Toolbar"
 import { useAnimalQuery } from "../queries"
-import NewEntry from "../components/new-entry/NewEntry"
 
 const AnimalProfilePage = () => {
     const { id } = useParams()
     const animalQuery = useAnimalQuery(id!)
 
     return (
-        <div>
-            {animalQuery.data && (
-                <div>
-                    <AnimalRecord data={animalQuery.data} />
-                    {animalQuery.data.exit_date && <NewEntry animalId={id} />}
-                </div>
-            )}
-        </div>
+        <ToolbarProvider>
+            <div>
+                {animalQuery.data && (
+                    <div>
+                        <AnimalRecord data={animalQuery.data} />
+                    </div>
+                )}
+                <Toolbar />
+            </div>
+        </ToolbarProvider>
     )
 }
 
