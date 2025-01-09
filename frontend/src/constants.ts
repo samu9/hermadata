f4506db2311b300cdc418f775d1a9c765cc12004
Samuele <samuelevf@gmail.com>
2025-01-09 13:40:30 +0100

    fe: add SeverityType enum for enhanced error handling

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,3 +1,12 @@
 export enum ApiErrorCode {
     existingChipCode = "ECC",
 }
+
+export type SeverityType =
+    | "success"
+    | "secondary"
+    | "info"
+    | "warning"
+    | "danger"
+    | "help"
+    | undefined
