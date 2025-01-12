c71b5c91871fdc4dcddaa58040d1ddf76dceb024
Samuele <samuelevf@gmail.com>
2025-01-12 10:23:30 +0100

    be: add user router with registration and login endpoints

---
+++ backend/hermadata/routers/user_router.py
@@ -0,0 +1,45 @@
+from typing import Annotated
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
+
+from hermadata.services.user_service import (
+    RegisterUserModel,
+    TokenData,
+    UserService,
+)
+from hermadata.initializations import get_current_user, user_service
+
+router = APIRouter(prefix="/user", tags=["user"])
+
+
+@router.post("/test")
+def test(
+    current_user: Annotated[TokenData, Depends(get_current_user)],
+):
+    return current_user
+
+
+@router.post("/register")
+def register(
+    data: RegisterUserModel,
+    service: Annotated[UserService, Depends(user_service)],
+):
+    service.register(data)
+
+    return True
+
+
+@router.post("/token")
+def login(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+    service: Annotated[UserService, Depends(user_service)],
+):
+
+    jwt = service.login(form_data.username, form_data.password)
+
+    if not jwt:
+        raise HTTPException(
+            status_code=400, detail="Incorrect username or password"
+        )
+
+    return {"access_token": jwt, "token_type": "bearer"}
