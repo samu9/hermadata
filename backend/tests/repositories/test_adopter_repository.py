2d5b254e55604a037baf0ded22b10e415a99b528
Samuele <samuelevf@gmail.com>
2025-02-21 15:56:34 +0100

    be: fix test

--- backend/tests/repositories/test_adopter_repository.py
+++ backend/tests/repositories/test_adopter_repository.py
@@ -2,6 +2,7 @@ from datetime import date
 
 from hermadata.repositories.adopter_repository import (
     AdopterSearchQuery,
+    IDDocumentType,
     NewAdopter,
     SQLAdopterRepository,
 )
@@ -17,6 +18,8 @@ def test_save_and_search(adopter_repository: SQLAdopterRepository):
             birth_date=date(1990, 1, 2),
             phone="123456",
             fiscal_code="AAAAAA12Z12Z123A",
+            document_number="AA12345AA",
+            document_type=IDDocumentType.driving_licence,
         )
     )
 
