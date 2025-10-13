70ad162b8afb9961d9027e708f9d2bf63c4ef2b9
Samuele <samuelevf@gmail.com>
2025-10-13 19:00:42 +0200

    feat: enhance LoggedUserCard with authentication checks and user information display

--- frontend/src/components/LoggedUserCard.tsx
+++ frontend/src/components/LoggedUserCard.tsx
@@ -1,10 +1,34 @@
-import { Avatar } from "primereact/avatar"
+import { Button } from "primereact/button"
+import { classNames } from "primereact/utils"
+import { useAuth } from "../contexts/AuthContext"
 
 const LoggedUserCard = () => {
+    const { logout, isAuthenticated, user, isSuperUser } = useAuth()
+
+    if (!isAuthenticated) {
+        return null
+    }
+
     return (
-        <div className="rounded-lg bg-slate-100 border border-slate-400 flex items-center justify-start gap-2 p-1">
-            <Avatar size="normal" />
-            <div className="text-xs">Username</div>
+        <div
+            className={classNames(
+                { "border-blue-400 bg-blue-100": isSuperUser },
+                "rounded-lg bg-slate-100 border border-slate-400 flex items-center justify-start gap-2 px-4"
+            )}
+        >
+            {/* <Avatar size="normal" /> */}
+            <div className="flex flex-col flex-grow">
+                <div className="text-xs font-medium">
+                    {user?.username || "Username"}
+                </div>
+            </div>
+            <Button
+                size="small"
+                text
+                severity="secondary"
+                onClick={logout}
+                label="Esci"
+            />
         </div>
     )
 }
