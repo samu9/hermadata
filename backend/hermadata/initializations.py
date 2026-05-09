e9899f4456745258d64ae8e6dcad473e576b6d8c
Samuele <samuelevf@gmail.com>
2026-05-09 18:11:27 +0200

    feat(therapy): add therapy repository initialization function

--- backend/hermadata/initializations.py
+++ backend/hermadata/initializations.py
@@ -20,6 +20,7 @@ from hermadata.repositories.city_repository import SQLCityRepository
 from hermadata.repositories.document_repository import SQLDocumentRepository
 from hermadata.repositories.race_repository import SQLRaceRepository
 from hermadata.repositories.structure_repository import SQLStructureRepository
+from hermadata.repositories.therapy_repository import SQLTherapyRepository
 from hermadata.repositories.user_repository import SQLUserRepository
 from hermadata.repositories.vet_repository import SQLVetRepository
 from hermadata.services.adopter_service import AdopterService
@@ -92,6 +93,12 @@ def get_activity_repository(
     session: Annotated[Session, Depends(get_db_session)],
 ) -> SQLActivityRepository:
     return SQLActivityRepository()(session)
+
+
+def get_therapy_repository(
+    session: Annotated[Session, Depends(get_db_session)],
+) -> SQLTherapyRepository:
+    return SQLTherapyRepository()(session)
 
 
 def get_structure_repository(
